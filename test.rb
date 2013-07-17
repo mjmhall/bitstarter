94cb205076038a1bda31289cfe2111d1413b3858
Martin Hall <mjm.hall@gmail.com>
2013-07-17 13:53:59 +0000

    remove test files

--- test.rb
+++
@@ -1,6 +0,0 @@
-cat hello.rb 
-class HiWorld
-  def self.hello
-    puts "Hello World"
-  end
-end
--- test1.rb
+++
@@ -1,6 +0,0 @@
-cat hello.rb 
-class HelloWorld
-  def self.hello
-    puts "Hello World"
-  end
-end
